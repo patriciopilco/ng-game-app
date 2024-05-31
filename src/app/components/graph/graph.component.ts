c2558055675c5c6cf9ce144f92c47737668c3077
Patricio Pilco <ppilco@galavail.com>
2024-05-31 16:54:19 -0500

    feat: Add chart container to graph component
    The code changes add a new CSS class called "chart-container" to the graph component. This class is used to create a grid layout for the chart container, improving the visual presentation of the graph.

--- src/app/components/graph/graph.component.ts
+++ src/app/components/graph/graph.component.ts
@@ -1,26 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
-  styleUrls: ['./graph.component.css']
+  styleUrls: ['./graph.component.css'],
 })
-export class GraphComponent {
-
-  results: any[]=[
-  {
-    "name": "Germany",
-    "value": 8940000
-  },
-  {
-    "name": "USA",
-    "value": 5000000
-  },
-  {
-    "name": "France",
-    "value": 7200000
-  }
-];
+export class GraphComponent  implements OnDestroy {
+  results: any[] = [
+    {
+      name: 'Germany',
+      value: 8940000,
+    },
+    {
+      name: 'USA',
+      value: 5000000,
+    },
+    {
+      name: 'France',
+      value: 7200000,
+    },
+  ];
 
   view: any[number] = [700, 400];
 
@@ -36,11 +35,24 @@ export class GraphComponent {
 
   colorScheme = 'nightLights';
 
-  constructor() {
+  intervalo;
 
+  constructor() {
+    this.intervalo = setInterval(() => {
+
+      const newResults = [...this.results];
+      for (let i = 0; i < newResults.length; i++) {
+        newResults[i].value = Math.floor(Math.random() * 500);
+      }
+      this.results = [...newResults];
+    }, 1000);
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.intervalo);
   }
 
-  onSelect(event:any) {
+  onSelect(event: any) {
     console.log(event);
   }
+
 }
